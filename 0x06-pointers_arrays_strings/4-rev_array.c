9b701e07292c2f4a899c3b83e808dd2b7a69884a
Amenyerdaw <amenyerdaw808@gmail.com>
2022-03-24 08:02:53 -0700

    My 4th task commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
  * reverse_array - reverses the content of an array of integers
  * @a: an array of integers
  * @n: number of elements of the array
- * Return: return void
+ * Return: returns void
  */
 
 void reverse_array(int *a, int n)
@@ -21,6 +21,7 @@ for (i = 0; i < n - 1; i++)
 {
 end_c++;
 }
+
 
 
 
