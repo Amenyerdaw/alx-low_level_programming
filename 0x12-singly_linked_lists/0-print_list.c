0e18acdb6982effaf197cb9bef94c33cb47e5ac6
Amenyerdaw <amenyerdaw808@gmail.com>
2022-04-22 00:08:54 -0700

    My 0 task commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 /**
- * print_list - prints all the elements  a list_t list.
+ * print_list - prints all the elements a list_t list.
+ *
  * @h: singly linked list.
+ *
  * Return: number of elements in the list.
  */
 size_t print_list(const list_t *h)
 {
+
 size_t nelem;
 nelem = 0;
 while (h != NULL)
