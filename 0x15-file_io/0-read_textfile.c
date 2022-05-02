97d9fe0131d1df99b060686ed3a11a523145446f
Amenyerdaw <amenyerdaw808@gmail.com>
2022-05-02 13:03:43 -0700

    My 0 task commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,37 @@
 #include "main.h"
-
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
- */
+* read_textfile - function
+* @filename: file to read
+* @letters: # of letters should read/print
+*
+* Description: func to read and print to stdout
+* Return: # of letters actually read/print
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+	char *buff;
+	int fd, wr;
+	ssize_t sz;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
-        return (0);
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+		return (0);
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0);
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
-        return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
-        return (0);
-    close(file);
-    free(totalSize);
-    return (fwrite);
+	buff = (char *)malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+	sz = read(fd, buff, letters);
+	if (sz == -1)
+		return (0);
+
+	buff[sz] = '\0';
+	wr = write(STDOUT_FILENO, buff, sz);
+	if (wr == -1)
+		return (0);
+
+	free(buff);
+	close(fd);
+	return (sz);
 }
